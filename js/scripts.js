eefb307547241f55a51a2e4509ee05af99a9d0e8
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-03-25 14:49:33 -0700

    add container class to ech form tag, add input tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+//business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -14,13 +17,41 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+// user interface (or front-end) logic:
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
-  event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  var result = add(number1, number2);
-  $("#output").text(result);
-
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result1 = add(number1, number2);
+    $("ul#output").text(result1);
   });
+
+  console.log("#add1", "#add2")
+
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result2 = subtract(number1, number2);
+    $("ul#output2").text(result2);
+  });
+
+  $("form#mult").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mult1").val());
+    var number2 = parseInt($("#mult2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+
+  $("form#div").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#output4").text(result);
+  });
+
 });
