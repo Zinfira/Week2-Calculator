8a7788b6158004644f72dd7da3a2bd5d27b7d3ea
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-03-25 13:23:06 -0700

    Add jumbotron class

--- js/scripts.js
+++ js/scripts.js
@@ -16,8 +16,11 @@ var divide = function(number1, number2) {
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
+  event.preventDefault();
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
-  alert(add(number1, number2));
+  var result = add(number1, number2);
+  $("#output").text(result);
+
   });
 });
