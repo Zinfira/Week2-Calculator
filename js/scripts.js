86cd9d7012c6752acb58725fb0c1e6b756ec6484
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-03-25 11:57:08 -0700

    add .submit function in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,11 @@ var multiply = function(number1, number2) {
 var divide = function(number1, number2) {
   return number1 / number2;
 };
-$(document).ready(function() {
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
 
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
   alert(add(number1, number2));
+  });
 });
